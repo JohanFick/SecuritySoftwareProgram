1982d6cddc29786c94bee15730acb82bb5c4b13e
Johan Fick <25867350@student.nwu.ac.za>
2017-10-24 23:16:55 +0200

    Update

--- build-Project_Shield_Software_Version1_1-Desktop_Qt_5_8_0_MinGW_32bit-Release/ui_mainwindow.h
+++ build-Project_Shield_Software_Version1_1-Desktop_Qt_5_8_0_MinGW_32bit-Release/ui_mainwindow.h
@@ -13,7 +13,6 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
-#include <QtWidgets/QFrame>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QLineEdit>
@@ -35,11 +34,10 @@ public:
     QLabel *MainText;
     QLabel *AddUserLabel;
     QPushButton *AddUser_Button;
-    QFrame *frame;
-    QLineEdit *UserName_LineEdit;
-    QLineEdit *UserPassWord_LineEdit;
-    QPushButton *LoginButton;
     QLabel *Info_Label;
+    QPushButton *LoginButton;
+    QLineEdit *UserPassWord_LineEdit;
+    QLineEdit *UserName_LineEdit;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
@@ -48,10 +46,11 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(800, 600);
+        MainWindow->resize(1336, 768);
         MainWindow->setAutoFillBackground(false);
         MainWindow->setStyleSheet(QLatin1String("QPushButton {\n"
-"color: #333;\n"
+"color:#333;\n"
+"background-color: light blue;\n"
 "border: 2px solid #555;\n"
 "border-radius: 11px;\n"
 "padding: 5px;\n"
@@ -108,7 +107,7 @@ public:
         Version_Text->setFont(font);
         MainText = new QLabel(centralWidget);
         MainText->setObjectName(QStringLiteral("MainText"));
-        MainText->setGeometry(QRect(200, 50, 561, 51));
+        MainText->setGeometry(QRect(450, 70, 561, 51));
         QFont font1;
         font1.setFamily(QStringLiteral("Javanese Text"));
         font1.setPointSize(20);
@@ -118,10 +117,10 @@ public:
         MainText->setFont(font1);
         AddUserLabel = new QLabel(centralWidget);
         AddUserLabel->setObjectName(QStringLiteral("AddUserLabel"));
-        AddUserLabel->setGeometry(QRect(660, 440, 161, 71));
+        AddUserLabel->setGeometry(QRect(1110, 510, 161, 71));
         AddUser_Button = new QPushButton(centralWidget);
         AddUser_Button->setObjectName(QStringLiteral("AddUser_Button"));
-        AddUser_Button->setGeometry(QRect(640, 510, 94, 31));
+        AddUser_Button->setGeometry(QRect(1090, 580, 94, 31));
         QFont font2;
         font2.setPointSize(10);
         font2.setBold(false);
@@ -129,86 +128,12 @@ public:
         font2.setWeight(50);
         font2.setStrikeOut(false);
         AddUser_Button->setFont(font2);
-        frame = new QFrame(centralWidget);
-        frame->setObjectName(QStringLiteral("frame"));
-        frame->setGeometry(QRect(260, 80, 331, 311));
-        QPalette palette;
-        QBrush brush(QColor(0, 0, 0, 255));
-        brush.setStyle(Qt::SolidPattern);
-        palette.setBrush(QPalette::Active, QPalette::WindowText, brush);
-        QBrush brush1(QColor(240, 21, 116, 255));
-        brush1.setStyle(Qt::SolidPattern);
-        palette.setBrush(QPalette::Active, QPalette::Button, brush1);
-        QBrush brush2(QColor(255, 127, 183, 255));
-        brush2.setStyle(Qt::SolidPattern);
-        palette.setBrush(QPalette::Active, QPalette::Light, brush2);
-        QBrush brush3(QColor(247, 74, 149, 255));
-        brush3.setStyle(Qt::SolidPattern);
-        palette.setBrush(QPalette::Active, QPalette::Midlight, brush3);
-        QBrush brush4(QColor(120, 10, 58, 255));
-        brush4.setStyle(Qt::SolidPattern);
-        palette.setBrush(QPalette::Active, QPalette::Dark, brush4);
-        QBrush brush5(QColor(160, 14, 77, 255));
-        brush5.setStyle(Qt::SolidPattern);
-        palette.setBrush(QPalette::Active, QPalette::Mid, brush5);
-        palette.setBrush(QPalette::Active, QPalette::Text, brush);
-        QBrush brush6(QColor(255, 255, 255, 255));
-        brush6.setStyle(Qt::SolidPattern);
-        palette.setBrush(QPalette::Active, QPalette::BrightText, brush6);
-        palette.setBrush(QPalette::Active, QPalette::ButtonText, brush);
-        palette.setBrush(QPalette::Active, QPalette::Base, brush6);
-        palette.setBrush(QPalette::Active, QPalette::Window, brush1);
-        palette.setBrush(QPalette::Active, QPalette::Shadow, brush);
-        QBrush brush7(QColor(247, 138, 185, 255));
-        brush7.setStyle(Qt::SolidPattern);
-        palette.setBrush(QPalette::Active, QPalette::AlternateBase, brush7);
-        QBrush brush8(QColor(255, 255, 220, 255));
-        brush8.setStyle(Qt::SolidPattern);
-        palette.setBrush(QPalette::Active, QPalette::ToolTipBase, brush8);
-        palette.setBrush(QPalette::Active, QPalette::ToolTipText, brush);
-        palette.setBrush(QPalette::Inactive, QPalette::WindowText, brush);
-        palette.setBrush(QPalette::Inactive, QPalette::Button, brush1);
-        palette.setBrush(QPalette::Inactive, QPalette::Light, brush2);
-        palette.setBrush(QPalette::Inactive, QPalette::Midlight, brush3);
-        palette.setBrush(QPalette::Inactive, QPalette::Dark, brush4);
-        palette.setBrush(QPalette::Inactive, QPalette::Mid, brush5);
-        palette.setBrush(QPalette::Inactive, QPalette::Text, brush);
-        palette.setBrush(QPalette::Inactive, QPalette::BrightText, brush6);
-        palette.setBrush(QPalette::Inactive, QPalette::ButtonText, brush);
-        palette.setBrush(QPalette::Inactive, QPalette::Base, brush6);
-        palette.setBrush(QPalette::Inactive, QPalette::Window, brush1);
-        palette.setBrush(QPalette::Inactive, QPalette::Shadow, brush);
-        palette.setBrush(QPalette::Inactive, QPalette::AlternateBase, brush7);
-        palette.setBrush(QPalette::Inactive, QPalette::ToolTipBase, brush8);
-        palette.setBrush(QPalette::Inactive, QPalette::ToolTipText, brush);
-        palette.setBrush(QPalette::Disabled, QPalette::WindowText, brush4);
-        palette.setBrush(QPalette::Disabled, QPalette::Button, brush1);
-        palette.setBrush(QPalette::Disabled, QPalette::Light, brush2);
-        palette.setBrush(QPalette::Disabled, QPalette::Midlight, brush3);
-        palette.setBrush(QPalette::Disabled, QPalette::Dark, brush4);
-        palette.setBrush(QPalette::Disabled, QPalette::Mid, brush5);
-        palette.setBrush(QPalette::Disabled, QPalette::Text, brush4);
-        palette.setBrush(QPalette::Disabled, QPalette::BrightText, brush6);
-        palette.setBrush(QPalette::Disabled, QPalette::ButtonText, brush4);
-        palette.setBrush(QPalette::Disabled, QPalette::Base, brush1);
-        palette.setBrush(QPalette::Disabled, QPalette::Window, brush1);
-        palette.setBrush(QPalette::Disabled, QPalette::Shadow, brush);
-        palette.setBrush(QPalette::Disabled, QPalette::AlternateBase, brush1);
-        palette.setBrush(QPalette::Disabled, QPalette::ToolTipBase, brush8);
-        palette.setBrush(QPalette::Disabled, QPalette::ToolTipText, brush);
-        frame->setPalette(palette);
-        frame->setFrameShape(QFrame::StyledPanel);
-        frame->setFrameShadow(QFrame::Raised);
-        UserName_LineEdit = new QLineEdit(frame);
-        UserName_LineEdit->setObjectName(QStringLiteral("UserName_LineEdit"));
-        UserName_LineEdit->setGeometry(QRect(70, 100, 171, 31));
-        UserPassWord_LineEdit = new QLineEdit(frame);
-        UserPassWord_LineEdit->setObjectName(QStringLiteral("UserPassWord_LineEdit"));
-        UserPassWord_LineEdit->setGeometry(QRect(70, 150, 171, 31));
-        UserPassWord_LineEdit->setEchoMode(QLineEdit::Password);
-        LoginButton = new QPushButton(frame);
+        Info_Label = new QLabel(centralWidget);
+        Info_Label->setObjectName(QStringLiteral("Info_Label"));
+        Info_Label->setGeometry(QRect(1190, 580, 94, 31));
+        LoginButton = new QPushButton(centralWidget);
         LoginButton->setObjectName(QStringLiteral("LoginButton"));
-        LoginButton->setGeometry(QRect(70, 220, 171, 31));
+        LoginButton->setGeometry(QRect(600, 340, 171, 31));
         LoginButton->setStyleSheet(QLatin1String("\n"
 "\n"
 "#Mainwindow LoginButton {\n"
@@ -233,13 +158,17 @@ public:
 "fx: 0.4, fy: -0.1,\n"
 "radius: 1.35, stop: 0 #fff, stop: 1 #ddd);\n"
 "}"));
-        Info_Label = new QLabel(centralWidget);
-        Info_Label->setObjectName(QStringLiteral("Info_Label"));
-        Info_Label->setGeometry(QRect(740, 510, 94, 31));
+        UserPassWord_LineEdit = new QLineEdit(centralWidget);
+        UserPassWord_LineEdit->setObjectName(QStringLiteral("UserPassWord_LineEdit"));
+        UserPassWord_LineEdit->setGeometry(QRect(600, 290, 171, 31));
+        UserPassWord_LineEdit->setEchoMode(QLineEdit::Password);
+        UserName_LineEdit = new QLineEdit(centralWidget);
+        UserName_LineEdit->setObjectName(QStringLiteral("UserName_LineEdit"));
+        UserName_LineEdit->setGeometry(QRect(600, 240, 171, 31));
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 800, 21));
+        menuBar->setGeometry(QRect(0, 0, 1336, 21));
         MainWindow->setMenuBar(menuBar);
         mainToolBar = new QToolBar(MainWindow);
         mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
@@ -255,25 +184,25 @@ public:
 
     void retranslateUi(QMainWindow *MainWindow)
     {
-        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "LoginWindow", Q_NULLPTR));
         label->setText(QString());
         Version_Text->setText(QApplication::translate("MainWindow", "Version1.1", Q_NULLPTR));
         MainText->setText(QApplication::translate("MainWindow", "PROJECT SHIELD SECURITY SOFTWARE", Q_NULLPTR));
         AddUserLabel->setText(QApplication::translate("MainWindow", "TextLabel", Q_NULLPTR));
         AddUser_Button->setText(QApplication::translate("MainWindow", "AddUser", Q_NULLPTR));
+        Info_Label->setText(QApplication::translate("MainWindow", "TextLabel", Q_NULLPTR));
 #ifndef QT_NO_TOOLTIP
-        UserName_LineEdit->setToolTip(QApplication::translate("MainWindow", "<html><head/><body><p>Enter UserName</p></body></html>", Q_NULLPTR));
+        LoginButton->setToolTip(QApplication::translate("MainWindow", "<html><head/><body><p>Click Login IF</p><p>UserName and Password was set</p></body></html>", Q_NULLPTR));
 #endif // QT_NO_TOOLTIP
-        UserName_LineEdit->setText(QApplication::translate("MainWindow", "admin", Q_NULLPTR));
+        LoginButton->setText(QApplication::translate("MainWindow", "Login", Q_NULLPTR));
 #ifndef QT_NO_TOOLTIP
         UserPassWord_LineEdit->setToolTip(QApplication::translate("MainWindow", "<html><head/><body><p>Enter Password</p></body></html>", Q_NULLPTR));
 #endif // QT_NO_TOOLTIP
         UserPassWord_LineEdit->setText(QApplication::translate("MainWindow", "admin", Q_NULLPTR));
 #ifndef QT_NO_TOOLTIP
-        LoginButton->setToolTip(QApplication::translate("MainWindow", "<html><head/><body><p>Click Login IF</p><p>UserName and Password was set</p></body></html>", Q_NULLPTR));
+        UserName_LineEdit->setToolTip(QApplication::translate("MainWindow", "<html><head/><body><p>Enter UserName</p></body></html>", Q_NULLPTR));
 #endif // QT_NO_TOOLTIP
-        LoginButton->setText(QApplication::translate("MainWindow", "Login", Q_NULLPTR));
-        Info_Label->setText(QApplication::translate("MainWindow", "TextLabel", Q_NULLPTR));
+        UserName_LineEdit->setText(QApplication::translate("MainWindow", "admin", Q_NULLPTR));
     } // retranslateUi
 
 };
